46e4587e00ebc3c5189ee88eec9e0d310bbeff41
Mouctar <Mr.conte@icloud.com>
2025-03-08 07:36:10 +0100

    Get

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,9 @@ const data = require("../frontend/public/data/data.json");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(express.json());
+
+app.get((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -16,8 +18,26 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/books", (req, res, next) => {
+app.get("/api/books", (req, res, next) => {
   res.status(200).json(data);
+});
+
+app.get("/livre/:id", (req, res, next) => {
+  const { id } = req.params;
+  const book = data.find((b) => b.id === id);
+
+  if (!book) {
+    return res.status(404).json({ message: "Livre non trouvé" });
+  }
+
+  res.status(200).json(book);
+});
+
+app.post("/api/books", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
 });
 
 module.exports = app;
