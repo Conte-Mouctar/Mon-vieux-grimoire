f1ed6d46130fd8508da0dc1ba0e82d278297d4a0
Mouctar <Mr.conte@icloud.com>
2025-03-27 18:19:18 +0100

    express validator

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -1,14 +1,33 @@
-const { checkSchema } = require("express-validator");
+const { checkSchema, validationResult } = require("express-validator");
 
-exports.validator = checkSchema({
+// Définition du schéma de validation
+const signupSchema = checkSchema({
   email: {
-    errorMessage: "Email invalide",
-    isEmail: true,
+    in: ["body"], // Assure que l'email vient du body
+    isEmail: {
+      errorMessage: "L'email n'est pas valide",
+    },
+    normalizeEmail: true,
   },
   password: {
+    in: ["body"],
     isLength: {
       options: { min: 8 },
-      errorMessage: "Le mot de passe doit contenir plus de 8 caractere",
+      errorMessage: "Le mot de passe doit contenir au moins 8 caractères",
     },
   },
 });
+
+// Middleware pour gérer les erreurs de validation
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+// Exportation des middlewares
+module.exports = {
+  signupValidator: [signupSchema, validate],
+};
