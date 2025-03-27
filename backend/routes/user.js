27ecad07bd5fc5dfcdc14b25f7c71a5924e47e26
Mouctar <Mr.conte@icloud.com>
2025-03-27 14:44:36 +0100

    projet mise a jour

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userCrtl = require("../controllers/user");
+const { connexionLimitee } = require("../middleware/rateLimit");
 
 router.post("/signup", userCrtl.signUp);
-router.post("/login", userCrtl.login);
+router.post("/login", connexionLimitee, userCrtl.login);
 
 module.exports = router;
