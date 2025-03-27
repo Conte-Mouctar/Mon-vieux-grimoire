82f4272f799d515cfd6697763a261631f10ac566
Mouctar <Mr.conte@icloud.com>
2025-03-27 15:23:21 +0100

    import validator commenteé

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userCrtl = require("../controllers/user");
 const { connexionLimitee } = require("../middleware/rateLimit");
+//const { validator } = require("../middleware/validator");
 
 router.post("/signup", userCrtl.signUp);
 router.post("/login", connexionLimitee, userCrtl.login);
